c6af8ab18cae6fe228729e938396527a92609488
hanbyeol.yang <beuri97@gmail.com>
2026-03-20 20:54:08 +0900

    feat: accept two non-negative integers as input

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,15 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner sc = new Scanner(System.in);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        int lhs = sc.nextInt();
+        int rhs = sc.nextInt();
+
+        System.out.println((lhs < 0 && rhs < 0) ? "잘못된 입력값입니다 0 포함 양의 정수를 입력하셔야 합니다." : "OK");
+
+        sc.close();
     }
 }
