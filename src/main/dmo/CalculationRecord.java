8ac7a1dfb688924d973cabe83be7ca3a1de7ef30
hanbyeol.yang <beuri97@gmail.com>
2026-03-25 15:32:57 +0900

    refactor: update record class
    - update int type to double
    - fix typo

--- src/main/dmo/CalculationRecord.java
+++ src/main/dmo/CalculationRecord.java
@@ -1,19 +1,21 @@
 package main.dmo;
 
+import main.enumerate.OperatorType;
+
 /**
  * A record class to store details of calculations
  * <br/>
  * 계산의 세부사항을 정리하기 위한 record class
  *
- * @param lfs left operand (연산자 기준 좌측 피연산자)
+ * @param lhs left operand (연산자 기준 좌측 피연산자)
  * @param rhs right operand (연산자 기준 우측 피연산자)
  * @param operator operator used in this calculation (계산에 사용된 연산자)
  * @param result the results of this calculation (이 계산의 결과)
  */
-public record CalculationRecord(int lfs, int rhs, char operator, int result) {
+public record CalculationRecord (double lhs, double rhs, OperatorType operator, double result) {
 
     @Override
     public String toString() {
-        return String.format("[%d %c %d = %d]", lfs, operator, rhs, result);
+        return String.format("[%.2f %c %.2f = %.2f]", lhs, operator.getSymbol(), rhs, result);
     }
 }
